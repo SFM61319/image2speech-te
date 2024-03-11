0ab02dddd814350ebcf90b3ec92b87f1d0f4a30d
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2024-03-11 13:28:34 +0530

    Refactor entrypoint

--- image2speech_te/__main__.py
+++ image2speech_te/__main__.py
@@ -2,22 +2,39 @@
 Main file of the project.
 """
 
+import logging
+import os
 
 from image2speech_te import audio, ocr, tts
 
 
-def main(argv: list[str]) -> int:
+def main(_argv: list[str]) -> int:
     """
     Entry point for the application.
     """
 
-    # Get the path to the input image and output audio files.
-    image_path = argv[1]
-    audio_path = argv[2]
+    IMAGE_PATH = "tests/assets/img"
+    AUDIO_PATH = "tests/target/aud"
 
-    text = ocr.read_text_from_image(image_path)
-    tts.convert_text_to_speech_audio(text, audio_path)
-    audio.play_speech_audio(audio_path)
+    AUDIO_EXT = ".mp3"
+    image_names = os.listdir(IMAGE_PATH)
+
+    if not os.path.exists(AUDIO_PATH):
+        os.makedirs(AUDIO_PATH)
+
+    for image_name in image_names:
+        audio_name = f"{image_name}{AUDIO_EXT}"
+
+        image_path = os.path.join(IMAGE_PATH, image_name)
+        audio_path = os.path.join(AUDIO_PATH, audio_name)
+
+        try:
+            text = ocr.read_text_from_image(image_path)
+            tts.convert_text_to_speech_audio(text, audio_path)
+            audio.play_speech_audio(audio_path)
+
+        except Exception as e:
+            logging.error(e)
 
     return 0
 
